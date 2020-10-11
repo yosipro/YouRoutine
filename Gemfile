3c6c1f06dd22c609f82befc9115dfd154727e269
yoshiaki.yahiro <yybuzinessonly@gmail.com>
2020-10-11 10:02:11 +0000

    for heroku2

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'carrierwave'
+gem 'fog-aws'
 gem 'mini_magick'
 gem 'kaminari'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_062336) do
+ActiveRecord::Schema.define(version: 2020_10_11_093058) do
+
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pickups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
